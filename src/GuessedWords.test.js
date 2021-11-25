ee796d0a56e5413b13c1172bd558c8b253a50b8f
JaysonChiang <jayson.chiang@outlook.com>
2021-11-26 00:01:10 +0800

    Tests and code for non-zero guessed words

--- src/GuessedWords.test.js
+++ src/GuessedWords.test.js
@@ -7,7 +7,7 @@ const setup = (props = {}) => shallow(<GuessedWord {...props} />);
 describe('if there are no words guessed', () => {
   let wrapper;
   beforeEach(() => {
-    wrapper = setup({ guessWords: [] });
+    wrapper = setup({ guessedWords: [] });
   });
   test('renders without error', () => {
     const component = findByTestAttr(wrapper, 'component-guessed-words');
@@ -19,4 +19,28 @@ describe('if there are no words guessed', () => {
   });
 });
 
-describe('if there are words guessed', () => {});
+describe('if there are words guessed', () => {
+  let wrapper;
+  const guessedWords = [
+    { guessedWord: 'train', letterMAtchCount: 3 },
+    { guessedWord: 'agile', letterMAtchCount: 1 },
+    { guessedWord: 'party', letterMAtchCount: 5 },
+  ];
+  beforeEach(() => {
+    wrapper = setup({ guessedWords });
+  });
+  test('renders without error', () => {
+    const component = findByTestAttr(wrapper, 'component-guessed-words');
+    expect(component.length).toBe(1);
+  });
+
+  test('render "guessed words" section', () => {
+    const guessedWordsNode = findByTestAttr(wrapper, 'guessed-words');
+    expect(guessedWordsNode.length).toBe(1);
+  });
+
+  test('correct number of guessed words', () => {
+    const guessedWordsNodes = findByTestAttr(wrapper, 'guessed-word');
+    expect(guessedWordsNodes.length).toBe(guessedWords.length);
+  });
+});
