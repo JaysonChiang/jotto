628175aab8e3641f9ec0c759bc2eff66db8e4164
JaysonChiang <jayson.chiang@outlook.com>
2021-12-06 21:04:46 +0800

    Add successReducer tests and code

--- src/reducers/successReducer.js
+++ src/reducers/successReducer.js
@@ -1,10 +1,18 @@
+import { actionTypes } from '../actions';
+
 /**
  * @function successReducer
  * @param {array} state
  * @param {object} action
- * @returns {boolean} 
-*/
-export default (state, action) => {
-  
-  return null;
-}
+ * @returns {boolean}
+ */
+const successReducer = (state = false, action) => {
+  switch (action.type) {
+    case actionTypes.CORRECT_GUESS:
+      return true;
+    default:
+      return state;
+  }
+};
+
+export default successReducer;
